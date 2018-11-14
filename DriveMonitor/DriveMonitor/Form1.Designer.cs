cc5ca3ed15c5558847919c99d165d0d623c1a2c0
MikeSifanele <45026640+MikeSifanele@users.noreply.github.com>
2018-11-14 08:36:05 +0200

    Add files via upload

---
+++ DriveMonitor/DriveMonitor/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace DriveMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.fileSystemWatcher2 = new System.IO.FileSystemWatcher();
+            this.fileSystemWatcher3 = new System.IO.FileSystemWatcher();
+            this.fileSystemWatcher4 = new System.IO.FileSystemWatcher();
+            this.fileSystemWatcher5 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.IncludeSubdirectories = true;
+            this.fileSystemWatcher1.NotifyFilter = ((System.IO.NotifyFilters)((((((System.IO.NotifyFilters.FileName | System.IO.NotifyFilters.DirectoryName) 
+            | System.IO.NotifyFilters.Attributes) 
+            | System.IO.NotifyFilters.LastWrite) 
+            | System.IO.NotifyFilters.LastAccess) 
+            | System.IO.NotifyFilters.CreationTime)));
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Created);
+            this.fileSystemWatcher1.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Deleted);
+            this.fileSystemWatcher1.Renamed += new System.IO.RenamedEventHandler(this.fileSystemWatcher1_Renamed);
+            // 
+            // fileSystemWatcher2
+            // 
+            this.fileSystemWatcher2.EnableRaisingEvents = true;
+            this.fileSystemWatcher2.IncludeSubdirectories = true;
+            this.fileSystemWatcher2.NotifyFilter = ((System.IO.NotifyFilters)((((((System.IO.NotifyFilters.FileName | System.IO.NotifyFilters.DirectoryName) 
+            | System.IO.NotifyFilters.Attributes) 
+            | System.IO.NotifyFilters.LastWrite) 
+            | System.IO.NotifyFilters.LastAccess) 
+            | System.IO.NotifyFilters.CreationTime)));
+            this.fileSystemWatcher2.SynchronizingObject = this;
+            this.fileSystemWatcher2.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher2_Created);
+            this.fileSystemWatcher2.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher2_Deleted);
+            this.fileSystemWatcher2.Renamed += new System.IO.RenamedEventHandler(this.fileSystemWatcher2_Renamed);
+            // 
+            // fileSystemWatcher3
+            // 
+            this.fileSystemWatcher3.EnableRaisingEvents = true;
+            this.fileSystemWatcher3.IncludeSubdirectories = true;
+            this.fileSystemWatcher3.NotifyFilter = ((System.IO.NotifyFilters)((((((System.IO.NotifyFilters.FileName | System.IO.NotifyFilters.DirectoryName) 
+            | System.IO.NotifyFilters.Attributes) 
+            | System.IO.NotifyFilters.LastWrite) 
+            | System.IO.NotifyFilters.LastAccess) 
+            | System.IO.NotifyFilters.CreationTime)));
+            this.fileSystemWatcher3.SynchronizingObject = this;
+            this.fileSystemWatcher3.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher3_Created);
+            this.fileSystemWatcher3.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher3_Deleted);
+            this.fileSystemWatcher3.Renamed += new System.IO.RenamedEventHandler(this.fileSystemWatcher3_Renamed);
+            // 
+            // fileSystemWatcher4
+            // 
+            this.fileSystemWatcher4.EnableRaisingEvents = true;
+            this.fileSystemWatcher4.IncludeSubdirectories = true;
+            this.fileSystemWatcher4.NotifyFilter = ((System.IO.NotifyFilters)((((((System.IO.NotifyFilters.FileName | System.IO.NotifyFilters.DirectoryName) 
+            | System.IO.NotifyFilters.Attributes) 
+            | System.IO.NotifyFilters.LastWrite) 
+            | System.IO.NotifyFilters.LastAccess) 
+            | System.IO.NotifyFilters.CreationTime)));
+            this.fileSystemWatcher4.SynchronizingObject = this;
+            this.fileSystemWatcher4.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher4_Created);
+            this.fileSystemWatcher4.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher4_Deleted);
+            this.fileSystemWatcher4.Renamed += new System.IO.RenamedEventHandler(this.fileSystemWatcher4_Renamed);
+            // 
+            // fileSystemWatcher5
+            // 
+            this.fileSystemWatcher5.EnableRaisingEvents = true;
+            this.fileSystemWatcher5.IncludeSubdirectories = true;
+            this.fileSystemWatcher5.NotifyFilter = ((System.IO.NotifyFilters)((((((System.IO.NotifyFilters.FileName | System.IO.NotifyFilters.DirectoryName) 
+            | System.IO.NotifyFilters.Attributes) 
+            | System.IO.NotifyFilters.LastWrite) 
+            | System.IO.NotifyFilters.LastAccess) 
+            | System.IO.NotifyFilters.CreationTime)));
+            this.fileSystemWatcher5.SynchronizingObject = this;
+            this.fileSystemWatcher5.Created += new System.IO.FileSystemEventHandler(this.fileSystemWatcher5_Created);
+            this.fileSystemWatcher5.Deleted += new System.IO.FileSystemEventHandler(this.fileSystemWatcher5_Deleted);
+            this.fileSystemWatcher5.Renamed += new System.IO.RenamedEventHandler(this.fileSystemWatcher5_Renamed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(677, 196);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Drive Monitor";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.IO.FileSystemWatcher fileSystemWatcher2;
+        private System.IO.FileSystemWatcher fileSystemWatcher3;
+        private System.IO.FileSystemWatcher fileSystemWatcher4;
+        private System.IO.FileSystemWatcher fileSystemWatcher5;
+    }
+}
+
---
+++ DriveMonitor/DriveMonitor/Form1.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.DirectoryServices.AccountManagement;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DriveMonitor
+{
+    public partial class Form1 : Form
+    {
+        StreamWriter myWriter;
+        FileInfo myFile;
+
+        DriveInfo[] myDrives = null;
+        string[,] myRegisteredDrives = new string[5, 2];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            register_drive();
+
+        }
+
+        public void register_drive()
+        {
+            try
+            {
+                myDrives = DriveInfo.GetDrives();
+
+                for (int i = 0; i < myDrives.Length; i++)
+                {
+
+                    if ((myRegisteredDrives[i, 0] == null || myRegisteredDrives[i, 0] == "") && (myDrives[i].DriveType.ToString().ToUpper() == "REMOVABLE" || myDrives[i].DriveType.ToString().ToUpper() == "CD-ROM"))
+                    {
+                        myRegisteredDrives[i, 0] = myDrives[i].RootDirectory.FullName;
+                        myRegisteredDrives[i, 1] = $"{myDrives[i].Name} {myDrives[i].DriveType}|Memory Available: {(Convert.ToDouble(myDrives[i].AvailableFreeSpace) / 1000000).ToString("f0")} MB|User: {Environment.UserName}|Time: {DateTime.Now}";
+                        Record_Drive($"Drive Inserted {myRegisteredDrives[i, 1]}");
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                myDrives = null;
+            }
+        }
+
+        public void update_drive_status()
+        {
+            try
+            {
+
+                myDrives = DriveInfo.GetDrives();
+
+                for (int i = 0; i < myRegisteredDrives.GetLength(0); i++)
+                {
+                    if (myRegisteredDrives[i, 0] != null && myRegisteredDrives[i, 0] != "")
+                    {
+                        if (!Directory.Exists(myRegisteredDrives[i, 0]))
+                        {
+                            myRegisteredDrives[i, 0] = "";
+
+                            if(myRegisteredDrives[i, 1] != null && myRegisteredDrives[i, 1] != "")
+                            {
+                                Record_Drive($"Drive Removed {myRegisteredDrives[i, 1]}");
+                            }                            
+
+                        }
+                        else if (myDrives[i].RootDirectory.FullName == myRegisteredDrives[i, 0])
+                        {
+                            myRegisteredDrives[i, 0] = myDrives[i].RootDirectory.FullName;
+                            myRegisteredDrives[i, 1] = $"{myDrives[i].Name} {myDrives[i].DriveType}|Memory Available: {(Convert.ToDouble(myDrives[i].AvailableFreeSpace) / 1000000).ToString("f0")} MB|User: {Environment.UserName}|Time: {DateTime.Now}";
+                        }
+
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                myDrives = null;
+            }
+
+        }
+
+        public void Record_Drive(string myDrive)
+        {
+            try
+            {
+                myWriter = new StreamWriter("Drive Activity Log.txt", true);
+
+                myWriter.WriteLine($"{myDrive}");
+
+            }
+            catch (Exception ex)
+            {
+                myWriter.Close();
+                MessageBox.Show(ex.Message, "Error - Logging Drive Activity");
+            }
+            finally
+            {
+                myDrives = null;
+                myWriter.Close();
+            }
+        }
+
+        public void Watch_Drives()
+        {
+            try
+            {
+                if (myRegisteredDrives[0, 0] != null)
+                {
+                    if (myRegisteredDrives[0, 0] != "")
+                    {
+                        fileSystemWatcher1.Path = myRegisteredDrives[0, 0];
+                        fileSystemWatcher1.EnableRaisingEvents = true;
+                    }
+                    else
+                    {
+                        fileSystemWatcher1.EnableRaisingEvents = false;
+                    }
+                }
+
+                if (myRegisteredDrives[1, 0] != null)
+                {
+                    if (myRegisteredDrives[1, 0] != "")
+                    {
+                        fileSystemWatcher2.Path = myRegisteredDrives[1, 0];
+                        fileSystemWatcher2.EnableRaisingEvents = true;
+                    }
+                    else
+                    {
+                        fileSystemWatcher2.EnableRaisingEvents = false;
+                    }
+                }
+
+                if (myRegisteredDrives[2, 0] != null)
+                {
+                    if (myRegisteredDrives[2, 0] != "")
+                    {
+                        fileSystemWatcher3.Path = myRegisteredDrives[2, 0];
+                        fileSystemWatcher3.EnableRaisingEvents = true;
+                    }
+                    else
+                    {
+                        fileSystemWatcher3.EnableRaisingEvents = false;
+                    }
+                }
+
+                if (myRegisteredDrives[3, 0] != null)
+                {
+                    if (myRegisteredDrives[3, 0] != "")
+                    {
+                        fileSystemWatcher4.Path = myRegisteredDrives[3, 0];
+                        fileSystemWatcher4.EnableRaisingEvents = true;
+                    }
+                    else
+                    {
+                        fileSystemWatcher4.EnableRaisingEvents = false;
+                    }
+                }
+
+                if (myRegisteredDrives[4, 0] != null)
+                {
+                    if (myRegisteredDrives[4, 0] != "")
+                    {
+                        fileSystemWatcher5.Path = myRegisteredDrives[4, 0];
+                        fileSystemWatcher5.EnableRaisingEvents = true;
+                    }
+                    else
+                    {
+                        fileSystemWatcher5.EnableRaisingEvents = false;
+                    }
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            register_drive();
+            update_drive_status();
+            Watch_Drives();
+        }
+       public void Record_Drive_Rename_Activity(RenamedEventArgs e)
+        {
+            try
+            {
+                myFile = new FileInfo(e.FullPath);
+                myWriter = new StreamWriter("Drive Activity Log.txt", true);
+
+                if (myFile.Attributes.ToString().Contains("Directory"))
+                {
+                    myWriter.WriteLine($"Folder {e.ChangeType}|Actioned by: {Environment.UserName}|Time: {DateTime.Now}|Old Folder Name: {e.OldFullPath.Split('\\')[e.FullPath.Split('\\').Length - 1]}|New Folder Name: {e.FullPath.Split('\\')[e.FullPath.Split('\\').Length - 1]}|Folder Path: {e.FullPath}");
+                }
+                else
+                {
+                    myWriter.WriteLine($"File {e.ChangeType}|Actioned by: {Environment.UserName}|Time: {DateTime.Now}|Old File Name: {e.OldFullPath.Split('\\')[e.FullPath.Split('\\').Length - 1]}|New File Name: {e.FullPath.Split('\\')[e.FullPath.Split('\\').Length - 1]}|File Path: {e.FullPath}");
+                }
+            }
+            catch (Exception ex)
+            {
+                myWriter.Close();
+                myFile = null;
+                MessageBox.Show(ex.Message, "Error - Logging drive activity");
+            }
+            finally
+            {
+                myWriter.Close();
+                myFile = null;
+                e = null;
+            }
+        }
+
+        public void Record_Drive_Activity(FileSystemEventArgs e)
+        {
+            try
+            {
+
+                myFile = new FileInfo(e.FullPath);
+                myWriter = new StreamWriter("Drive Activity Log.txt", true);
+
+                if (myFile.Attributes.ToString().Contains("Directory"))
+                {
+                    myWriter.WriteLine($"Folder {e.ChangeType}|Actioned by: {Environment.UserName}|Time: {DateTime.Now}|Folder Name: {e.FullPath.Split('\\')[e.FullPath.Split('\\').Length - 1]}|Folder Path: {e.FullPath}");
+                }
+                else
+                {
+                    myWriter.WriteLine($"File {e.ChangeType}|Actioned by: {Environment.UserName}|Time: {DateTime.Now}|File Name: {e.FullPath.Split('\\')[e.FullPath.Split('\\').Length - 1]}|File Path: {e.FullPath}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                myWriter.Close();
+                myFile = null;
+                MessageBox.Show(ex.Message, "Error - Loggin drive activity");
+            }
+            finally
+            {
+                myWriter.Close();
+                myFile = null;
+                e = null;
+            }
+        }
+
+        private void fileSystemWatcher1_Created(object sender, FileSystemEventArgs e)
+        {
+            Record_Drive_Activity(e);
+        }
+
+        private void fileSystemWatcher1_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Record_Drive_Activity(e);
+        }
+
+        private void fileSystemWatcher1_Renamed(object sender, RenamedEventArgs e)
+        {
+            Record_Drive_Rename_Activity(e);
+        }
+
+        private void fileSystemWatcher2_Created(object sender, FileSystemEventArgs e)
+        {
+            Record_Drive_Activity(e);
+        }
+
+        private void fileSystemWatcher2_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Record_Drive_Activity(e);
+        }
+
+        private void fileSystemWatcher2_Renamed(object sender, RenamedEventArgs e)
+        {
+            Record_Drive_Rename_Activity(e);
+        }
+
+        private void fileSystemWatcher3_Created(object sender, FileSystemEventArgs e)
+        {
+            Record_Drive_Activity(e);
+        }
+
+        private void fileSystemWatcher3_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Record_Drive_Activity(e);
+        }
+
+        private void fileSystemWatcher3_Renamed(object sender, RenamedEventArgs e)
+        {
+            Record_Drive_Rename_Activity(e);
+        }
+
+        private void fileSystemWatcher4_Created(object sender, FileSystemEventArgs e)
+        {
+            Record_Drive_Activity(e);
+        }
+
+        private void fileSystemWatcher4_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Record_Drive_Activity(e);
+        }
+
+        private void fileSystemWatcher4_Renamed(object sender, RenamedEventArgs e)
+        {
+            Record_Drive_Rename_Activity(e);
+        }
+
+        private void fileSystemWatcher5_Created(object sender, FileSystemEventArgs e)
+        {
+            Record_Drive_Activity(e);
+        }
+
+        private void fileSystemWatcher5_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Record_Drive_Activity(e);
+        }
+
+        private void fileSystemWatcher5_Renamed(object sender, RenamedEventArgs e)
+        {
+            Record_Drive_Rename_Activity(e);
+        }
+    }
+}
